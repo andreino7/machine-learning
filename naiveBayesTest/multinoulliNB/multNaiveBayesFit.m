20d556ef7500f840d68c0efd6cd61f591951a27e
Ronnypetson <rsronnypetson4@gmail.com>
2015-11-23 15:00:26 -0800

    Update multNaiveBayesFit.m

--- naiveBayesTest/multinoulliNB/multNaiveBayesFit.m
+++ naiveBayesTest/multinoulliNB/multNaiveBayesFit.m
@@ -1,7 +1,8 @@
 function model = multNaiveBayesFit(X, y, m)
 %%  This function fits a multinoulli Naive Bayes model
 % m is the number of classes
-% X(i, j) is the feature j value on the sample i
+% X(i, j) is the feature j value on the sample i.
+%  The values are enumerated from 1;
 % y(i) is the class label of the i-th sample
 % model is the model to be fitted
 %
@@ -9,8 +10,8 @@ function model = multNaiveBayesFit(X, y, m)
     N = S(1); % Number of samples
     D = S(2); % Dimension of each sample
     classSizes = zeros(1, m);
-    classPrior = zeros(1, N);
-    MNVALUES = 20; % The maximum number of values for
+    classPrior = zeros(1, m);
+    MNVALUES = 20; % The maximum number of possible values for
                    % each feature is 20
     featureParams = zeros(D, m, MNVALUES);
     
@@ -19,7 +20,7 @@ function model = multNaiveBayesFit(X, y, m)
         classSizes(c) = classSizes(c) + 1;
         classPrior(c) = classPrior(c) + 1;
         for j = 1:D
-            att = X(i, j) + 1; % because the enumeration starts at 1
+            att = X(i, j);
             featureParams(j, c, att) = featureParams(j, c, att) + 1;
         end
     end
