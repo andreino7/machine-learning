39da3fcfd5b1c880145f8b6168c5fd1908d7bb73
Ronnypetson <rsronnypetson4@gmail.com>
2015-11-28 19:52:54 -0800

    Update GaussianNaiveBayesPredict.m

--- naiveBayesTest/GaussianNB/GaussianNaiveBayesPredict.m
+++ naiveBayesTest/GaussianNB/GaussianNaiveBayesPredict.m
@@ -1,10 +1,11 @@
 function y = GaussianNaiveBayesPredict(X, model, m)
-%%  This function predicts with Naive Bayes classifier for multi-valued features
+%%  This function predicts with Naive Bayes classifier for continuous and multi-valued features
 % m is the number of classes
 % x(i, j) is the feature j on sample i
 % model is the NB model fitted previously
 % y(i) is the class label for the i-th sample
 %
+    % X(:,[4]) = [];
     S = size(X);
     N = S(1); % Number of samples
     D = S(2); % Dimension of each sample
@@ -21,10 +22,21 @@ function y = GaussianNaiveBayesPredict(X, model, m)
                 att = X(i, j);
                 mean_jc = featureParams(j, c, 1);
                 sqDev_jc = featureParams(j, c, 2);
-                logLik = -0.5*(att - mean_jc).^2/sqDev_jc - log(sqrt(sqDev_jc));
+                if j > 4 % Binary features
+                    if att == 1
+                        logLik = log(mean_jc);
+                    else
+                        logLik = log(1.0 - mean_jc);
+                    end
+                else % Continuous or multi-valued features
+                    diff2 = (att - mean_jc).^2;
+                    logLik = (-0.5*diff2/sqDev_jc) - log(sqrt(sqDev_jc*6.2832));
+                end
                 L(i, c) = L(i, c) + logLik;
             end
             
+            % a = L(i, c);
+            % b = L(i, argmax_c);
             if L(i, c) > L(i, argmax_c)
                 argmax_c = c;
             end
@@ -32,7 +44,3 @@ function y = GaussianNaiveBayesPredict(X, model, m)
         y(i) = argmax_c;
     end
 end
-
-% Quantify qualitative features
-% Discretize continuous features
-% Use boosting
